e5dcb33992cf8a4fde35e6fb728f24fabcce95fd
DickensJuma <dickensjuma13@gmail.com>
2020-10-20 13:02:22 +0300

    node mailer

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Theme
+    theme: "darkSky",
+
+    // Email notifier account (sender)
+    host: "mail.winkeyinternational.co.ke", // Sender email smtp
+    port: "465", // Sender email port
+    user: "admin@winkeyinternational.co.ke", // Sender email username
+    pass: "Vb5)iOWdXS+w", // Sender email password
+
+    // Your email to receive notification (receiver)  
+    from: 'info@winkeyinternational.co.ke', // Sender email address
+    to: 'techsupport@winkeyinternational.co.ke', // Your email address
+    subject: 'Contact Us', // Subject
+};
